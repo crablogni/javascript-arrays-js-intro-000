650ba3369eab7c92a0446165e77eda3b9ede9c40
hector ariceaga <hector_ariceaga@hotmail.com>
2018-02-23 16:44:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -23,8 +23,8 @@ function accessElementInArray(array,index){
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
-  array.shift();
-  return array;
+  array.unshift()
+  return array
 }
 
 function removeElementFromBeginningOfArray(array){
