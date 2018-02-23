834a13f06fe5f5d1616386f60a56a3642cdb08c3
hector ariceaga <hector_ariceaga@hotmail.com>
2018-02-23 16:47:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,7 +31,7 @@ function removeElementFromBeginningOfArray(array){
   return array.slice(1);
 }
 
-function destructivelyRemoveElementFromBeginningOfArray(array){
+function destructivelyRemoveElementFromEndOfArray(array){
   array.pop();
   return array;
 }
